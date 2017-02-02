2023b71c6f033283a2786fd1c820e1924a6ec94e
GIRAUD Maxime <maxime.giraud@openmailbox.org>
2017-02-02 11:14:24 +0100

    hello users

--- db/migrate/20170202101238_create_users.rb
+++ db/migrate/20170202101238_create_users.rb
@@ -1,10 +1,10 @@
 class CreateUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :users do |t|
-      t.string :nick
+      t.string :nike
       t.string :firstname
       t.string :lastname
-      t.string :password_digest
+      t.string :password
 
       t.timestamps
     end
